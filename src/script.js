80f100a06df60c04699f2b2dd65351175d2f89ba
gael <76548719+gaelzarco@users.noreply.github.com>
2023-03-09 12:57:18 -0800

    buffer geometry is awesome

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 /**
  * Base
  */
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -11,8 +12,39 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 2, 3)
+
+//Float32Array
+// const positionsArray = new Float32Array(9)
+// positionsArray[0] = 0
+// positionsArray[1] = 0
+// positionsArray[2] = 0
+
+// positionsArray[3] = 0
+// positionsArray[4] = 1
+// positionsArray[5] = 0
+
+// positionsArray[6] = 1
+// positionsArray[7] = 0
+// positionsArray[8] = 0
+
+const count = 500
+
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for (let i = 0; i < count * 3 * 3; i++) {
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position', positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({
+        color: 0xff0000, 
+        wireframe: true
+    })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -22,6 +54,7 @@ const sizes = {
     height: window.innerHeight
 }
 
+// Resize
 window.addEventListener('resize', () =>
 {
     // Update sizes
